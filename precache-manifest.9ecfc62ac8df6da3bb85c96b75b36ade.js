8a093d82cfbcd0745c2b57cb10eec2fc88fd6bb9
nparida3 <61738434+nparida3@users.noreply.github.com>
2020-07-12 20:13:53 +0530

    Updates

---
+++ precache-manifest.9ecfc62ac8df6da3bb85c96b75b36ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43b1508c598e9d9ee422ac10e2918fe",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "a766f15a9cabb8c62203",
+    "url": "/seasons/static/css/main.b2bb7d44.chunk.css"
+  },
+  {
+    "revision": "07ff909f942daecebc25",
+    "url": "/seasons/static/js/2.6cdfe8f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.6cdfe8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a766f15a9cabb8c62203",
+    "url": "/seasons/static/js/main.99467a5e.chunk.js"
+  },
+  {
+    "revision": "09da936633b5906b4cb6",
+    "url": "/seasons/static/js/runtime-main.d5462e81.js"
+  }
+]);
